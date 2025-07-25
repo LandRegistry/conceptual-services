815bb3f4bf0ac00c7dea358b2d7d6ecfdb3a6b54
Andy Porter <andy.porter@digital.landregistry.gov.uk>
2025-07-25 11:49:29 +0100

    initil version of app

---
+++ README.md
@@ -0,0 +1,41 @@
+Conceptual Services - screenshots and brief notes
+================
+
+Browse screenshots of conceptual services, for land and property services
+
+You can view different sets of screenshots for each exemplar, zoom in and out of them or step through them in order.
+
+
+## Running this app locally
+
+The app is very simple and doesn't require anything to be installed or run in a terminal. Just open the index.html file in the root folder. It works especially well in Chrome's presentation mode. Your mileage may vary in other browsers.
+
+## examples we want to conceptualise and add here include:
+
+- CCA
+- commonhold
+- find information about property - citizen
+- services for information - organised around group/need
+- services a citizen dould do themselves, like afer a death of a change of name, and any other ideas?
+- permission to do something, that needs information about property - an owner or someone with a right getting permission, or giving another person access
+- geospatial ideas - what could a geostpatial register do / allow?
+- digital casework - if public services changed how would casework change to also suppoert that / work in the future
+
+
+## How to contribute
+
+If you work on conceptualising what could be a truly digital vision - without current constraints.  I.e. what would a service be like now, if starting from sratch.
+
+
+### 1. Add your images
+
+Each conceptaul service has it's own numbered folder in '/exemplar'. Drop all your images into the 'images' subfolder (1024px wide is best).
+
+### 2. Update the data file
+
+Edit the data.js file in your numbered exemplar folder. It contains a few examples so you can see how the data should be structured.
+
+### 3. Update the index file
+
+Once you're done, don't forget to add a link to your exemplar from the 'index.html' file in the root of this repository (not the one in your numbered exemplar folder).
+
---
+++ exemplar/5/data.js
@@ -0,0 +1,63 @@
+var data = {
+  "service": "5. Land registry",
+  "last-updated": "6 January 2015",
+  "userjourneys": [            
+    {
+      "title": "Get property information",
+      "path": [{
+        "caption": "Start and search",
+        "imgref": "images/get-property-information/1.Find-property-information.png",
+      },{
+        "caption": "Select property",
+        "imgref": "images/get-property-information/2.Select-property.png",
+        "note": "Search results."
+      },{
+        "caption": "Property information",
+        "imgref": "images/get-property-information/3.5-Granary-Avenue-Dorchester-DT14YY.png",
+        "note": [
+        	"A page for every registered property on GOV.UK.",
+        	"Anyone can view this without payment or verifying their identity."
+        	]
+      },{
+        "caption": "Verify identity",
+        "imgref": "images/get-property-information/4.Verify-identity.png",
+        "note": "Uses GOV.UK verify."
+      },{
+        "caption": "Payment details",
+        "imgref": "images/get-property-information/5.Payment-details.png",
+        "note": "Payment may not be required."
+      },{
+        "caption": "Register view",
+        "imgref": "images/get-property-information/6.Official-register-of-title-DT122047.png",
+        "note": [
+        	"Other versions below.",
+        	"This is not an official copy of the register."
+        	]
+      }
+      
+      ]
+    },{
+      "title": "Variations of the register vew",
+      "path": [{
+        "caption": "Alpha final version",
+        "imgref": "images/register-views/A.Official-register.of-title-DT122047.png"
+      },{
+        "caption": "Earlier version",
+        "imgref": "images/register-views/B.Official-register-of-title-DT122047.png"
+      },{
+        "caption": "With collapsible bits",
+        "imgref": "images/register-views/C.Official-register-of-title-DT122047.png"
+      },{
+        "caption": "Using the existing ABC register structure",
+        "imgref": "images/register-views/D.Official-register-of-title-DT122047.png"
+      },{
+        "caption": "Readable register",
+        "imgref": "images/register-views/E.Land-Ownership-record-MARS0002.png",
+        "note": "Created on LR Hackday."
+      },{
+        "caption": "Changes to register",
+        "imgref": "images/register-views/Z.Changes-to-register-of-title-DT122047.png"
+      }
+      ]
+    }
+  ]}
