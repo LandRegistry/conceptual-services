faeff192f4eaccbb68c3eaa27306ace3cfcdb2c7
Martin Coulthurst <mc@lightness.co.uk>
2025-08-05 14:40:40 +0100

    Add geospatial examples

---
+++ exemplar/31/data.js
@@ -0,0 +1,58 @@
+var data = {
+  "service": "Geospatial",
+  "last-updated": "5 August 2025",
+  "userjourneys": [            
+    {
+      "title": "Updated title search",
+      "path": [{
+        "caption": "Ordnance Survey product viewer beta",  
+        "imgref": "images/OS-Beta.png",
+        "note": [
+          "Build on the existing work: Geospatial test and learn, Local land charges, as well as other government projects to create modern, accessibly maps."
+        ]
+      },{
+        "caption": "Defra map for flooding",
+        "imgref": "images/DEFRA-map.png",
+        "note": "Example of an accessible, user-friendly map"
+      },
+      ]
+    },
+    {
+      "title": "Planning checks",
+      "path": [{
+        "caption": "Geospatial check of development planning",  
+        "imgref": "images/check-planning.png",
+        "note": [
+          "Carry out a search for planning purposes, either directly or as part of planning application."
+        ]
+      },{
+        "caption": "Title numbers",
+        "imgref": "images/check-titles.png",
+        "note": "Present title information, possibly LLC or CCA data."
+      },{
+        "caption": "Defra map for flooding",
+        "imgref": "images/check-flooding.png",
+        "note": "Use the same search area to return flood map data from Defra's service."
+      },{
+        "caption": "Defra Nature Restoration Fund",
+        "imgref": "images/check-NRF.png",
+        "note": [
+          "Similarly, carry out a search using geospatial data using a Nature Restoration Fund service.",
+          "For checks against national infrastructure using HSE data, we may not want to show a map at this stage but flag if futher investigation is required."
+
+        ]
+      }
+      ]
+    },
+    {
+      "title": "Boundary rationalisation",
+      "path": [{
+        "caption": "Title boundaries",  
+        "imgref": "images/boundaries.png",
+        "note": [
+          ""
+        ]
+      }
+      ]
+    }
+  ]}
