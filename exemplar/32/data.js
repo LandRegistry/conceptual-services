2f5fd9d322e464238dfb8eec73640d088685550b
Martin Coulthurst <mc@lightness.co.uk>
2025-08-27 19:00:06 +0100

    Add example on modernising the various web services

---
+++ exemplar/32/data.js
@@ -0,0 +1,64 @@
+var data = {
+  "service": "Marginal gains",
+  "last-updated": "25 August 2025",
+  "userjourneys": [            
+    {
+      "title": "Data visualisation",
+      "path": [{
+        "caption": "Internal tools",  
+        "imgref": "images/AP-register-asis.png",
+        "note": [
+          "Follow user-centered design principles to make improvements across internal case work systems.",
+          "Identify the most complex areas through UR and analytics, eg replace the current register display to improve the clarity of the data"
+        ]
+      },{
+        "caption": "Comparison of Register data",
+        "imgref": "images/AP-register-comparison.png",
+        "note": [
+          "Using typography to improve the presentation of data.",
+          "Follow Tufte's design principles to remove unnecessary information and add emphasis to the key elements of the data.",
+        ]
+      },{
+        "caption": "Conceptual Application processing",
+        "imgref": "images/AP-transfer.png",
+        "note": [
+          "Redesign the UI in order to streamline the application and present the necessary data in one place for users.",
+          "eg, display register and individual documents side by side for comparison, and track the ones that match; or display title maps and street views where the application calls for eh definition of the property type",
+        ]
+      }
+      ]
+    },
+    {
+      "title": "User accounts",
+      "path": [{
+        "caption": "Improve the user experience with user accounts",  
+        "imgref": "images/one-login.png",
+        "note": [
+          "Implement GOV.UK One Login to enable users to access HMLR servicesu using an existing, validated identity.",
+          "Investigate organisational accounts to serve customers with multiple teams members sharing access to services, eg LLC,",
+        ]
+      }
+      ]
+    },
+    {
+      "title": "Modernise web services",
+      "path": [{
+        "caption": "Upgrade to modern web standards",  
+        "imgref": "images/portal_signin.png",
+        "note": [
+          "Upgrade Portal to modern web standards for accessibilty and usability.",
+          "Use a modern digital-first web font (eg Roboto as per Portal's homepage) and consider a brand refresh. As a default web safe font, Arial can appear generic and make HMLR appear less intentional with its design choices.",
+          "Design for modern devices and screen sizes, eg mobile, tablet and desktop"
+          
+        ]
+      },{
+        "caption": "Existing message area",
+        "imgref": "images/portal_messages.png",
+        "note": [
+          "Establish consistency of page layout and components, eg introducing new header and footer components, to work in a similar manner to other gov sites. ",
+          "Review the site's navigation."
+        ]
+      }
+      ]
+    }
+  ]}
